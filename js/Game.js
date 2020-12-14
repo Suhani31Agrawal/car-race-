bc7c52698df647906bb5bea82d1168bc1e4fc573
! Agrawal <n-1519-11@dpssurat.net>
2020-12-14 22:31:07 +0530

    ooh

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1image)
     car2 = createSprite(300,200);
+    car2.addImage(car2image)
     car3 = createSprite(500,200);
+    car3.addImage(car3image)
     car4 = createSprite(700,200);
+    car4.addImage(car4image)
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,14 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("brown")
+      //imageMode(CENTER)
+      image(trackimage,0,-displayHeight*4,displayWidth,displayHeight*5)
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 180;
       var y;
 
       for(var plr in allPlayers){
@@ -56,11 +62,11 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 250;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
+         cars[index-1].x = x;
+         cars[index-1].y = y;
 
         if (index === player.index){
           cars[index - 1].shapeColor = "red";
@@ -79,6 +85,14 @@ class Game {
       player.update();
     }
 
+    if(player.distance>4000){
+      gameState=2;
+    }
+
     drawSprites();
+  }
+
+ end(){
+console.log("gameoverrrr!!")
   }
 }
--- sketch.js
+++ sketch.js
@@ -8,8 +8,16 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
+var cars, car1, car2, car3, car4,car1image,car2image,car3image,car4image;
+var trackimage;
 
+function preload(){
+  car1image=loadImage("images/car1.png")
+  car2image=loadImage("images/car2.png")
+  car3image=loadImage("images/car3.png")
+  car4image=loadImage("images/car4.png")
+  trackimage=loadImage("images/track.jpg")
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
