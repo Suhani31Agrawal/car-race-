a337800cce3bb5393b4b9ed1f54b0529f7d32dcf
! Agrawal <n-1519-11@dpssurat.net>
2021-01-04 21:59:30 +0530

    car racer

--- js/Game.js
+++ js/Game.js
@@ -69,6 +69,9 @@ class Game {
          cars[index-1].y = y;
 
         if (index === player.index){
+          stroke(5)
+          fill("yellow")
+          ellipse(x,y,100,100)
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
